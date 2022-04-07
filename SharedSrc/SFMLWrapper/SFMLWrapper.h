e5967a4a8701f9c4d64c5c2780d397549c34f30c
Deon Khong <72732410+deonkhong@users.noreply.github.com>
2022-04-07 11:34:23 +0800

    update

--- SharedSrc/SFMLWrapper/SFMLWrapper.h
+++ SharedSrc/SFMLWrapper/SFMLWrapper.h
@@ -13,42 +13,56 @@ namespace DOTL
 {
 	struct MouseEventData
 	{
-		bool pressed_ { false };
-		float x_ , y_;
-		float click_radius_ { 40.0f };
+		bool pressed_{ false };
+		float x_, y_;
+		float click_radius_{ 40.0f };
+	};
+
+	struct SFMLSprite
+	{
+		SFMLSprite( char const* name );
+
+		void SetPosition( float x, float y );
+
+		void SetScale( float x, float y );
+
+		sf::Sprite& GetSprite();
+
+		//sf::Texture GetTexture();
+
+	private:
+		sf::Texture texture_;
+		sf::Sprite sprite_;
 	};
 
 	struct SFMLInstance
 	{
-		SFMLInstance ( int windowWidth , int windowHeight , char const* name );
+		SFMLInstance( int windowWidth, int windowHeight, char const* name );
 
-		void PollMouseEvents ( MouseEventData& med );
+		void PollMouseEvents( MouseEventData& med );
 
-		void Update ( GameData& data , float dt , uint16_t playerID , uint16_t targetID );
+		void Update( GameData& data, float dt, uint16_t playerID, uint16_t targetID );
 
-		bool IsOpen ();
+		void Draw();
+
+		bool IsOpen();
 
 	private:
 		std::shared_ptr<sf::RenderWindow> window_;
 		sf::Font font_;
 		sf::Text text_;
 
-		static constexpr int font_size_ { 18 };
-		static constexpr int font_offset_ { 25 };
-	};
+		SFMLSprite background_{ "Assets/Arts/Background_v1_20220403.png" };
+		SFMLSprite tower_pink_{ "Assets/Arts/Tower_Pink_v1_20220403.png" };
+		SFMLSprite tower_blue_{ "Assets/Arts/Tower_Blue_v1_20220403.png" };
+		SFMLSprite player_pink_{ "Assets/Arts/Player_Pink_v1_20220403.png" };
+		SFMLSprite player_blue_{ "Assets/Arts/Player_Blue_v1_20220403.png" };
+		SFMLSprite minion_pink_{ "Assets/Arts/Minion_Pink_v1_20220403.png" };
+		SFMLSprite minion_blue_{ "Assets/Arts/Minion_Blue_v1_20220403.png" };
+		SFMLSprite bullet_pink_{ "Assets/Arts/Bullet_Pink_v1_20220403.png" };
+		SFMLSprite bullet_blue_{ "Assets/Arts/Bullet_Blue_v1_20220403.png" };
 
-	struct SFMLSprite
-	{
-		SFMLSprite ( char const* name );
-
-		void SetPosition ( float x , float y );
-
-		void SetRotation ( float deg );
-
-		void SetScale ( float x , float y );
-
-	private:
-		sf::Texture texture_;
-		sf::Sprite sprite_;
+		static constexpr int font_size_{ 18 };
+		static constexpr int font_offset_{ 25 };
 	};
 }
